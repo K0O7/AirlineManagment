2020f2441ad9e0b516e9f52e7226519e42d3c4cc
K0O7 <piotrdulinski25@gmail.com>
2024-11-15 18:11:43 +0100

    Update AirlineManager.java

--- src/airline/AirlineManager.java
+++ src/airline/AirlineManager.java
@@ -26,3 +26,11 @@ public class AirlineManager {
 		System.out.print("works");
 	}
 }
+
+
+/*
+Add 5 interfaces to the existing hierarchy.
+Use polymorphism with the abstract class and interface from the hierarchy.
+Create final class, method, variable. 
+Create a static block, method, variable. 
+*/
